f03252afecb7707d7e761109a734023943eade18
Ray Berger <rlberger@eckerd.edu>
2017-08-15 09:59:55 -0400

    add use strict, remove all instances of var

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,45 +1,47 @@
+'use strict';
 const debugging = (window == top);
-window.onload = function() {
+let scheduledEmails;
+window.onload = function () {
     displaySchedule();
     displayDrafts();
     displayEmailAddress();
 
     // Set dates to today
-    var curTime = moment().format('HH:mm');
-    var curDate = moment().format('YYYY-MM-DD');
+    const curTime = moment().format('HH:mm');
+    const curDate = moment().format('YYYY-MM-DD');
     document.querySelector('#date').value = curDate;
     document.querySelector('#time').value = curTime;
 
     // Adds date picker for browsers without date support
     loadPicker();
 
-    setTimeout(function(){
+    setTimeout(function () {
         document.getElementById('myHTML').style.height = '100%';
         document.querySelector('#myHTML > body').style.height = '100%';
-    }, 1000)
-    /* NOTE 1: This is very hacky but what is happening is #myHTML must have height set to over 100% otherwise you can't scroll on mobile (if needed)
+    }, 100)
+    /* 
+    NOTE 1: This is very hacky but what is happening is #myHTML must have height set to over 100% otherwise you can't scroll on mobile (if needed)
     This may be caused by this all being in an iframe and a lot of the height of the document rendering later.
     This basically sets the height back to 100% like normal
-        */
+    */
 };
 
-function getEmailAddress(callback){
-    if (debugging){
-        let address = "email@email.com";
-        callback(address);
+function getEmailAddress(callback) {
+    if (debugging) {
+        callback("email@email.com");
     } else {
         google.script.run.withSuccessHandler(callback).getCurrentUser();
     }
 }
-function displayEmailAddress(){
-    getEmailAddress(function(address){
+function displayEmailAddress() {
+    getEmailAddress(function (address) {
         document.querySelector('#userEmail').innerText = address;
     })
 }
-function getDrafts(callback){
-    if (debugging){
+function getDrafts(callback) {
+    if (debugging) {
         let draftSubjects = ["Subject 1", "Subject 2", "etc", "Medium Article Examples", "Resume Inspiration",
-         "iPod Print", "https://iwantwhatitsworth.com/","Lo And Behold: Reveries Of The Connected World"];
+            "iPod Print", "https://iwantwhatitsworth.com/", "Lo And Behold: Reveries Of The Connected World"];
         callback(draftSubjects);
     } else {
         google.script.run.withSuccessHandler(callback).getDraftSubjects();
@@ -47,8 +49,8 @@ function getDrafts(callback){
 }
 function displayDrafts() {
     getDrafts(function (drafts) {
-        var options = '';
-        for (var draft of drafts) {
+        let options = '';
+        for (let draft of drafts) {
             options += '<option value="' + draft + '">' + draft + '<\/option>';
         }
         document.querySelector('#subjects').innerHTML = options;
@@ -58,7 +60,7 @@ function displayDrafts() {
 function getSchedule(callback) {
     showLoading(true);
     if (debugging) {
-        if (typeof(scheduledEmails) == "undefined"){
+        if (typeof (scheduledEmails) == "undefined") {
             scheduledEmails = [
                 { "subject": "Resume Inspiration", "date": "2018-08-01T20:11:00.000Z" },
                 { "subject": "iPod Print", "date": "2018-07-31T21:14:00.000Z" },
@@ -73,13 +75,13 @@ function getSchedule(callback) {
 function displaySchedule() {
     getSchedule(
         function (s) {
-            var template = document.querySelector('#productrow');
-            var newContents = document.createDocumentFragment();
+            const template = document.querySelector('#productrow');
+            let newContents = document.createDocumentFragment();
 
             for (let message of s) {
-                var formattedDate = moment(new Date(message.date)).format('MMM Do YYYY, hh:mm a');
-                var clone = document.importNode(template.content, true);
-                var td = clone.querySelectorAll("td");
+                const formattedDate = moment(new Date(message.date)).format('MMM Do YYYY, hh:mm a');
+                const clone = document.importNode(template.content, true);
+                let td = clone.querySelectorAll("td");
                 td[0].textContent = message.subject;
                 td[1].textContent = formattedDate;
                 td[2].addEventListener('click', function () { removeScheduledMessage(message.subject); });
@@ -91,24 +93,23 @@ function displaySchedule() {
             document.querySelector("tbody").appendChild(newContents);
             showLoading(false);
         }
-        //function(s){console.log("hi")}
     )
 }
 function addScheduledMessage() {
-    let date = document.querySelector('#date').value;
-    let time = parseTime(document.querySelector('#time').value);
-    let subject = document.querySelector('#subjects').value;
+    const date = document.querySelector('#date').value;
+    const time = parseTime(document.querySelector('#time').value);
+    const subject = document.querySelector('#subjects').value;
     let sendDate = new Date(date + " " + time);
 
-    if (dateIsInFuture(sendDate)){
+    if (dateIsInFuture(sendDate)) {
         sendDate = sendDate.toJSON();
         showError(false);
-        if (debugging){
+        if (debugging) {
             console.log("Submitting the following to Google: " + subject + sendDate);
-            let newEmail = { "subject": subject, "date": sendDate };
-            let index = scheduledEmails.findIndex(e => {return e.subject == subject});
+            const newEmail = { "subject": subject, "date": sendDate };
+            const index = scheduledEmails.findIndex(e => { return e.subject == subject });
             index > -1 ? scheduledEmails[index] = newEmail : scheduledEmails.push(newEmail);
-        } else{
+        } else {
             google.script.run.addEmailToSchedule(subject, sendDate);
         }
         displaySchedule();
@@ -117,60 +118,56 @@ function addScheduledMessage() {
     }
 }
 function removeScheduledMessage(subject) {
-    document.querySelectorAll('td').forEach(e => {if (e.innerText == subject) {e.parentNode.remove()}});
-    if (debugging){
+    document.querySelectorAll('td').forEach(e => { if (e.innerText == subject) { e.parentNode.remove() } });
+    if (debugging) {
         console.log("Removing: " + subject);
-    } else{
+    } else {
         google.script.run.removeEmailFromSchedule(subject);
     }
 }
-
-function loadPicker(){
+function loadPicker() {
     // Only loads if browser doesn't support input type="date"
-    var dateField = document.createElement("input");
+    let dateField = document.createElement("input");
     dateField.setAttribute("type", "date");
 
     if (dateField.type != "date") {
-        var head = document.querySelector('#myHTML > head');
-        var script = document.createElement("script");
+        let head = document.querySelector('#myHTML > head');
+        let script = document.createElement("script");
         script.src = "https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/pikaday.js";
         head.appendChild(script);
 
-        var css = document.createElement("link");
-        css.setAttribute("rel","stylesheet");
-        css.setAttribute("type","text/css");
+        let css = document.createElement("link");
+        css.setAttribute("rel", "stylesheet");
+        css.setAttribute("type", "text/css");
         css.href = "https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css";
         head.appendChild(css);
 
-        setTimeout(function(){
-            var picker = new Pikaday({ field: document.getElementById('date') });
+        setTimeout(function () {
+            const picker = new Pikaday({ field: document.getElementById('date') });
         }, 2000)
     }
 }
 function dateIsInFuture(formDate) {
-    return moment(formDate).isAfter(new Date());
+    return moment(formDate).isAfter(Date.now());
 }
-
-function parseTime(string){
+function parseTime(string) {
     //Necessary to avoid adding another library for cross browser time support
-    var isPM = string.toLowerCase().includes("p");
-    var numbers = string.match(/\d+/g);
-    var hours = parseInt(numbers[0]);
-    var minutes = parseInt(numbers[1] | 0);
+    const isPM = string.toLowerCase().includes("p");
+    const numbers = string.match(/\d+/g);
+    let hours = parseInt(numbers[0]);
+    const minutes = parseInt(numbers[1] | 0);
     if (isPM && hours != 12) hours += 12;
     return hours + ":" + minutes;
 }
-
-function showLoading(boolean){
-    if (boolean){
+function showLoading(boolean) {
+    if (boolean) {
         document.querySelector('#loader').classList.add('loader');
     } else {
         document.querySelector('#loader').classList.remove('loader');
     }
 }
-
 function showError(boolean) {
-    if (boolean){
+    if (boolean) {
         document.querySelector('#dateTimeRow').classList.add("has-error");
         document.querySelector('.help-block').classList.remove("hidden");
     } else {
